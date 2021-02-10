863477b50499d8c27cfefe5186ef45da74f01878
“amatoosan” <amatoosan9525@gmail.com>
2021-02-10 23:49:40 +0900

    ログイン前ヘッダー作成

--- front/react-front/src/containers/Heder.jsx
+++ front/react-front/src/containers/Heder.jsx
@@ -1,9 +1,51 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
+// images
+import MainLogo from '../images/icon.jpg';
+
+const HeaderWrapper = styled.div`
+  height: 80px;
+  width: 100%;
+  background-color: #292929;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+`;
+const MainLogoImage = styled.img`
+  height: 60px;
+  margin: 10px 10px;
+  float: left;
+`;
+const SiteIntroduction = styled.p`
+  margin-top: 25px;
+  color: #fff;
+  float: left;
+`;
+const Button = styled.a`
+  width: 80px;
+  text-align: center;
+  background-color: rgb(78, 78, 78);
+  float: right;
+  line-height: 80px;
+  margin-right: 2px;
+  list-style: none;
+  display: table;
+  text-decoration: none;
+`;
 
 export const Heder = () => {
   return (
     <Fragment>
-      ヘッダーです
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+        <SiteIntroduction>ようこそ！</SiteIntroduction>
+          <Button href="">ゲスト<br />ログイン</Button>
+          <Button href="/login">ログイン</Button>
+          <Button href="/signup">サインイン</Button>
+          <Button href="">検索</Button>
+          <Button href="/">ホーム</Button>
+      </HeaderWrapper>
     </Fragment>
   )
 }
